4ffb37ca187fac4fc477c728ee05f32d058c8764
karthikm73 <121124166+karthikm73@users.noreply.github.com>
2023-06-08 12:14:49 +0530

    Create 5_dateFormats.cpp

---
+++ 5_dateFormats.cpp
@@ -0,0 +1,43 @@
+ #include<iostream>
+ #include<cstdio>
+ using namespace std;
+ 
+ class date
+ {  
+   int day,month,year;
+   public: 
+          date(char *d);
+          date(int m,int d,int y);
+          void show_date();
+          
+ };
+ 
+ //initialize using string
+ date::date(char *d)
+ {
+   sscanf(d, "%d%*c%d%*c%d", &month, &day, &year);
+ }
+ 
+ //initaialize using integers 
+ date::date(int m,int d,int y)
+ {
+   day=d;
+   month=m;
+   year=y;
+ }
+ 
+ void date::show_date()
+ {
+   cout<<month<<"/"<<day;
+   cout<<"/"<<year<<"\n";
+   
+ }
+ 
+ int main()
+ {
+   date ob1(12,4,2003),ob2("10/22/2003");
+   ob1.show_date();
+   ob2.show_date();
+   
+   return 0;
+}
